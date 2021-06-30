9b438de2896742c8b609195bb014d11d5d42a19c
PiotrOwczarek1 <PiotrOwczarek1@users.noreply.github.com>
2021-06-30 22:57:05 +0200

    heroku clientId

--- src/auth.js
+++ src/auth.js
@@ -2,7 +2,7 @@ const OktaJwtVerifier = require('@okta/jwt-verifier');
 
 const oktaJwtVerifier = new OktaJwtVerifier({
   //clientId: '0oa14998duT0uWSTB5d7',
-  //clientId: '0oa14982z7Qv7Yhwo5d7',
+  clientId: '0oa14982z7Qv7Yhwo5d7',
   issuer: 'https://dev-8156823.okta.com/oauth2/default'
 });
 
